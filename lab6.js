825d2b18963156ef9ecc64a130756ccdef0afd80
AngelJGit <angel2630gabriel@gmail.com>
2024-10-09 09:31:00 -0400

    CPAN 134 Lab-6

---
+++ lab6.js
@@ -0,0 +1,53 @@
+
+// //to run each individual task, uncomment the section 
+
+
+// //Task 1
+// let myNumber = 10; //declaring a variable named myNumber and giving it a value of 10 
+
+// let myString ="Hello, JavaScript!" //declaring a variable names myString and giving it a value of "Hello, JavaScript!"
+
+// console.log(myNumber) //Output: 10
+// console.log(myString) //Output: Hello, JavaScript!
+
+
+// // Task 2 PART 1
+// for (let i = 0; i <= 10; i++) {
+//     console.log(i);
+// }
+// // This block declared a variable "i" and gives it a value of 0. 
+// // <= 10 means that the loop will keep running until it reached loop 10.
+// // i++ essentialy means i = i + 1, therefore i will keep increasing by 1 until it reaches 10 where "i <= 10" will stop the increment 
+
+// // TASK 2 PART 2
+// for (let i = 1; i <= 5; i++){
+//     console.log(3 * i);
+// }
+// // The block declared a varialble name "i" and gave it a value of 1 
+// // i <= 5 means that the loop will run 5 times 
+// // i++ will continue the loop properly, until it reaches the 5th loop
+
+
+// // Task 3
+// function addNumbers(num1, num2) {
+//     return num1 + num2;
+// }
+
+// let result = addNumbers(4, 10); 
+// console.log(result); //output: 14
+
+// // the block declares a function variable named addNumbers
+// // (num1, num2) are placeholders for the values that will be passed in to the function when it is called 
+// // the return line is where the operation is performed. (num1 + num2)
+ 
+// function multiplyNumbers(num3, num4) {
+//     return num3 * num4
+// }
+
+// let results = multiplyNumbers(5, 10);
+// console.log (results);
+
+// // the block declared a function to multiplyNumbers
+// // (num3, num4) are placeholders for the values that the function will be given to execute 
+// // the return is where the operation is told what to do / is where the code is performed, it tell the function 
+// // to do multiplication (*)
